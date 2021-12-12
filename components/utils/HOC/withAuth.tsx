23732fce42c4bc5a2bee3027a94d03ba2af46c19
Dekguh <kadekteguhmahesa@gmail.com>
2021-12-12 18:57:28 +0800

    fix component type at withAuth

--- components/utils/HOC/withAuth.tsx
+++ components/utils/HOC/withAuth.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import { NextPage } from 'next'
 import { useCookies } from 'react-cookie'
 
-const withAuth = <T extends any,>(Component : React.Component<T> | NextPage) => {
+const withAuth = <T extends any,>(Component : React.ComponentType<T> | NextPage) => {
     return (props : any) => {
         const Router = useRouter()
         const [cookies, setCookie, removeCookie] = useCookies(['jwtDasbora'])
