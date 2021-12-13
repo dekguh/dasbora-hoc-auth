d06d6871e92937231b81837b1241958d12a7916d
Dekguh <kadekteguhmahesa@gmail.com>
2021-12-13 13:57:35 +0800

    fix component name error

--- components/utils/HOC/withAuth.tsx
+++ components/utils/HOC/withAuth.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import { NextPage } from 'next'
 import { useCookies } from 'react-cookie'
 
-const withAuth = <T extends any,>(Component : React.ComponentType<T> | NextPage) => {
+const withAuth = <T extends any,>(Component : React.ComponentType<T> | NextPage) : React.FC<T> => {
     return (props : any) => {
         const Router = useRouter()
         const [cookies, setCookie, removeCookie] = useCookies(['jwtDasbora'])
